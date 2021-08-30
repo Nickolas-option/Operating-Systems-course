590dcc65d33e9e324db06c375fba1e60c9a65100
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-08-30 23:07:52 +0300

    Update ex1.c

--- week02/ex1.c
+++ week02/ex1.c
@@ -1,1 +1,15 @@
-
+#include "limits.h"
+#include "stdio.h"
+#include "float.h"
+int main() {
+        int a;
+        float b;
+        double c;
+        a = INT_MAX;
+        b = FLT_MAX;
+        c = DBL_MAX;
+        printf("%d\n%f\n%lf\n", a, b, c);
+        printf("Size of integer: %lu\n", sizeof(a));
+        printf("Size of float: %lu\n", sizeof(b));
+        printf("Size of double: %lu\n", sizeof(c));
+}
