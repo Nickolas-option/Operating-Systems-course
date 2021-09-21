94783de20f6d151aaeb39f645f4847de7de492a2
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-09-21 23:53:40 +0300

    Update ex1.c

--- week05/ex1.c
+++ week05/ex1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
 
+void *myThread(void *args) {
+    //code
+
+    printf("pid = %d\n", (int)(intptr_t) args);
+}
+
+int main() {
+    pthread_t thread[10];
+    int i = 0;
+    while (i < 10) {
+    if (pthread_create((thread+i), NULL, myThread, (void*)(intptr_t) i)) {
+        printf(">>ERROR IN CREATING Thread #%d!\n", i);
+        pthread_exit(NULL);
+        return 1;
+    } else {
+        pthread_join(thread[i], NULL);
+        i++;
+        printf(" >>Thread created!\n");
+    }
+
+    }
+    i = 0;
+ return 0;
+}
