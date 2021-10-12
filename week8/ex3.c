0118266549eaa04704bbe651d1c9e9705128573b
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-10-12 23:43:37 +0300

    Create ex3.c

---
+++ week8/ex3.c
@@ -0,0 +1,15 @@
+/* top - 23:42:47 up 12 min,  0 users,  load average: 0.07, 0.03, 0.00
+Tasks:   6 total,   2 running,   4 sleeping,   0 stopped,   0 zombie
+%Cpu(s):  0.4 us,  0.0 sy,  0.0 ni, 99.6 id,  0.0 wa,  0.0 hi,  0.0 si,  0.0 st
+MiB Mem :  15846.9 total,  15085.0 free,    680.6 used,     81.3 buff/cache
+MiB Swap:   4096.0 total,   4096.0 free,      0.0 used.  14965.4 avail Mem
+
+  PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND
+   91 nikolas   20   0  616912 615528   1112 S   1.5   3.8   0:00.09 ex2
+    1 root      20   0     936    596    488 S   0.0   0.0   0:00.03 init
+    7 root      20   0     936    124     16 S   0.0   0.0   0:00.00 init
+    8 root      20   0     936    124     16 R   0.0   0.0   0:00.12 init
+    9 nikolas   20   0   10568   5492   3220 S   0.0   0.0   0:00.13 bash
+   92 nikolas   20   0   10872   3736   3224 R   0.0   0.0   0:00.00 top */
+
+// The process consumes the memory
