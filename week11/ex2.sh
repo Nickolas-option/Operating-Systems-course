ae9de986b182e44c30e14a6796ecf9a737971da9
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-11-03 01:37:10 +0300

    Create ex2.sh

---
+++ week11/ex2.sh
@@ -0,0 +1,39 @@
+sudo chmod 777 lofsdisk/ 
+cd lofsdisk/
+sudo touch file1
+sudo touch file2
+sudo chmod 777 file1
+sudo chmod 777 file2  
+sudo echo "Nikolas" > file1
+sudo echo "Evkarpidi" > file2
+
+#Adding shared libraries
+mkdir bin
+mkdir lib
+mkdir lib64
+mkdir lib/x86_64-linux-gnu
+
+cp -v /bin/bash ./bin
+cp -v /bin/cat ./bin
+cp -v /bin/echo ./bin
+cp -v /bin/ls ./bin
+
+list="$(ldd /bin/bash | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/bash | egrep -o '/lib64.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/cat | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/cat | egrep -o '/lib64.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/echo | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/echo | egrep -o '/lib64.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/ls | egrep -o '/lib.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+list="$(ldd /bin/ls | egrep -o '/lib64.*\.[0-9]')"
+for i in $list; do cp  -v "$i" ".${i}"; done
+
+gcc ../ex2.c -o ex2.out
+sudo chroot . ./ex2.out > ../ex2.txt
