ccb52bde634d734e2c483182f14d137713180c7e
Nickolas-option <69462766+Nickolas-option@users.noreply.github.com>
2021-11-03 01:33:55 +0300

    Create ex2.c

---
+++ week11/ex2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+typedef struct dirent dirent;
+
+int main() {
+    DIR *pDir;
+    dirent *pDirent;
+    pDir = opendir("/");
+
+    if (pDir == NULL) {
+        printf("Cannot open root directory");
+        return EXIT_SUCCESS;
+    }
+    printf("Root content:\n");    
+    pDirent = readdir(pDir);
+    while (pDirent != NULL) {
+        printf ("%s\n", pDirent->d_name);
+        pDirent = readdir(pDir);
+    }
+    
+    closedir(pDir);
+    return EXIT_SUCCESS;
+}
